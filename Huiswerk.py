5c02661728d32ca63b8e82ad67429e93d4c25a49
T_L <thijmen.langwerden1@gmail.com>
2026-06-05 10:02:58 +0200

    Update Huiswerk.py

--- Huiswerk.py
+++ Huiswerk.py
@@ -135,7 +135,7 @@ THEMES = {
 # INSTELLINGEN & CONFIGURATIE
 # ============================================================
 
-HUIDIGE_VERSIE = "7.0.9"
+HUIDIGE_VERSIE = "7.0.9v"
 GITHUB_VERSION_URL = "https://raw.githubusercontent.com/thijmenlangwerden1-hub/GC-OS/main/version.txt"
 GITHUB_SCRIPT_URL = "https://raw.githubusercontent.com/thijmenlangwerden1-hub/GC-OS/refs/heads/GC-OS/Huiswerk.py"
 GITHUB_CHANGELOG_URL = "https://raw.githubusercontent.com/thijmenlangwerden1-hub/GC-OS/refs/heads/GC-OS/changelog.txt"
